6dd08ed638970cf9441b8216cea17f023b350010
Amanda <amcooksey@system76-pc.localdomain>
2022-08-02 11:02:39 -0700

    changed how samples are assoc with gffcompare query numbers

--- makeUNILNCmapping.sh
+++ makeUNILNCmapping.sh
@@ -5,6 +5,14 @@
 
 tracking="unified_lncrna_ids.tracking"
 
+while getopts 't:' option
+do
+  case "${option}" in
+    t) tissue=${OPTARG};;
+    \?) echo "No legal parameters were passed. Please run with -h parameter to see help"; exit 1;;
+esac
+done
+
 grep 'Summary' unified_lncrna_ids.stats > stattmp.txt
 sed -i 's/\#\= Summary for dataset\: //' stattmp.txt
 sed -i 's/_candidate_lncRNA.gtf.lncRNA.gtf//' stattmp.txt
@@ -14,24 +22,27 @@ readarray -t tissuearray < stattmp.txt
 
 rm stattmp.txt
 
-for i in "${!tissuearray[@]}"
+for i in "${!tissuearray[@]}";
 do
-        tissue=${tissuearray[$i]}
-        touch "$tissue"_lncRNA_mapping.txt "$tissue"_temp.txt
-        query=$(( 1 + $i ))
-        column=$(( $i + 5 ))
-        grep q"$query" unified_lncrna_ids.tracking | cut -f 1,2,$column > "$tissue"_temp.txt
-        sed -i 's/q[0-9]://g' "$tissue"_temp.txt
-        cat "$tissue"_temp.txt | while read -r unilnc xloc id remainder
-        do
-                gene=$(echo $id | cut -f3 | cut -d '|' -f1)
-                lnc=$(echo $id | cut -f3 | cut -d '|' -f2)
-		if [[ "$gene" != "-" ]]
-			then
-				echo -e "$unilnc\t$xloc\t$gene\t$lnc"
-			        echo -e "$unilnc\t$xloc\t$gene\t$lnc" >> "$tissue"_lncRNA_mapping.txt
-		fi
- 	done
-        rm "$tissue"_temp.txt
+    if [[ "${tissuearray[$i]}" = "${tissue}" ]];
+    then
+        index=$i
+        break
+    fi
 done
 
+touch "$tissue"_lncRNA_mapping.txt "$tissue"_temp.txt
+query=$(( 1 + $index ))
+column=$(( $index + 5 ))
+grep q"$query" unified_lncrna_ids.tracking | cut -f 1,2,$column > "$tissue"_temp.txt
+sed -i 's/q[0-9]://g' "$tissue"_temp.txt
+cat "$tissue"_temp.txt | while read -r unilnc xloc id remainder
+do
+       	gene=$(echo $id | cut -f3 | cut -d '|' -f1)
+       	lnc=$(echo $id | cut -f3 | cut -d '|' -f2)
+	if [[ "$gene" != "-" ]]
+		then
+		        echo -e "$unilnc\t$xloc\t$gene\t$lnc" >> "$tissue"_lncRNA_mapping.txt
+	fi
+done
+rm "$tissue"_temp.txt
